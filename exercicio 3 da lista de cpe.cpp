8815e5f5fcde2ae7713814cd2ce6e7df94a77787
audinex <124537555+audinex@users.noreply.github.com>
2024-04-01 22:37:31 -0300

    Add files via upload

---
+++ exercicio 3 da lista de cpe.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int r;
+	
+	cout << "escolha um raio" << endl;
+	cin >> r;
+	
+	cout << "diametro da circuferencia: " << 2*3.14159*r << endl << "a area da circuferencia: " << 3.14159*r*r << endl;
+	
+	return 0;
+	
+} 
