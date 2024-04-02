a985208f7d1f151f1d5f30e1ede8f72287ebc88b
audinex <124537555+audinex@users.noreply.github.com>
2024-04-01 22:19:40 -0300

    Add files via upload

---
+++ exercicio 2 de cpe.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a;
+	int b;
+	
+	cout<< "insira dois numeros" << endl;
+	
+	cin>> a;
+    cin>> b;
+	
+	cout<< "soma de dos dois numeros " << a + b << endl << "produto dos numeros " << a * b << endl << "e o quociente " << a / b << endl;
+	return 0;
+	
+} 
